5f08d9a2a0a43f6cefb1db080e5ae88e2aa8d9bd
Brent Reininger <brent.reininger89@gmail.com>
2023-01-09 17:17:47 -0800

    Add login post route

--- app.js
+++ app.js
@@ -46,6 +46,21 @@ app.post("/register", function (req, res) {
   });
 });
 
+app.post("/login", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({ email: username }, function (err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser.password === password) {
+        res.render("secrets");
+      }
+    }
+  });
+});
+
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
